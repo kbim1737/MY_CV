d35318ebd6d1724b35ee41c2441e6b847f4cd7f8
Kovacs Bela Richard <richy199898@gmail.com>
2020-09-10 16:18:10 +0300

    Updates

---
+++ precache-manifest.f3fc4d28e9522af4fbeb5448aa8206ef.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6b9d7a93ff7a8e5793d915378061fda",
+    "url": "/MY_CV/index.html"
+  },
+  {
+    "revision": "fb499744c6658cf59c1c",
+    "url": "/MY_CV/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "60d727248a6778b22d94",
+    "url": "/MY_CV/static/js/2.f4a7f79a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/MY_CV/static/js/2.f4a7f79a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb499744c6658cf59c1c",
+    "url": "/MY_CV/static/js/main.4b434789.chunk.js"
+  },
+  {
+    "revision": "e443e61859b51a8220a8",
+    "url": "/MY_CV/static/js/runtime-main.bf3dbb77.js"
+  },
+  {
+    "revision": "25f10eb3ab1daa8be7156cabf7bd765d",
+    "url": "/MY_CV/static/media/cambridge.25f10eb3.png"
+  },
+  {
+    "revision": "31b4d1af4e60ef84f31fdb68907e19f1",
+    "url": "/MY_CV/static/media/ecdl.31b4d1af.jpg"
+  },
+  {
+    "revision": "244681e754a3bafc535b0a6d8cba7bb3",
+    "url": "/MY_CV/static/media/eng.244681e7.jpg"
+  },
+  {
+    "revision": "fe65f75110975550798c83354612b164",
+    "url": "/MY_CV/static/media/kep.fe65f751.jpg"
+  },
+  {
+    "revision": "863a68720166f2b7a908d10814f1b9fd",
+    "url": "/MY_CV/static/media/kolcsey.863a6872.jpg"
+  },
+  {
+    "revision": "b20211a838dc686921deedd9516cf561",
+    "url": "/MY_CV/static/media/ubb.b20211a8.png"
+  }
+]);
